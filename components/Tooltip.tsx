57951b08526f039cc2793eb199f55b0cfbbc771d
Faiyaz Shaikh <www.shtakkar@gmail.com>
2023-08-19 18:39:29 +0530

    tooltip on mobile as well

--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -1,10 +1,9 @@
-import React, { PropsWithChildren, useContext } from 'react'
+import React, { PropsWithChildren } from 'react'
 import Tippy from '@tippyjs/react'
 import 'tippy.js/dist/tippy.css' // optional
 import 'tippy.js/dist/border.css' // optional
 import 'tippy.js/dist/svg-arrow.css' // optional
 import 'tippy.js/dist/backdrop.css' // optional
-import ApplicationContext from './ApplicationContext'
 
 interface ITooltipProps extends PropsWithChildren {
   content: React.ReactNode
@@ -13,12 +12,7 @@ interface ITooltipProps extends PropsWithChildren {
 const Tooltip: React.FC<ITooltipProps> = props => {
   const { content, children } = props
 
-  const applicationContext = useContext(ApplicationContext)
-  const {
-    device: { isMobile },
-  } = applicationContext
-
-  if (!content || isMobile) {
+  if (!content) {
     return <>{children}</>
   }
 

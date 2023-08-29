7dff27b5dc9bab36415226547581e2936b06cfa5
Faiyaz Shaikh <www.shtakkar@gmail.com>
2023-08-29 18:46:57 +0530

    fix: header: 'X-Content-Type-Options'

--- next.config.js
+++ next.config.js
@@ -55,10 +55,10 @@ const securityHeaders = [
     key: 'X-Frame-Options',
     value: 'SAMEORIGIN',
   },
-  // {
-  //   key: 'X-Content-Type-Options',
-  //   value: 'nosniff',
-  // },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
   {
     key: 'Strict-Transport-Security',
     value: 'max-age=63072000; includeSubDomains; preload',
